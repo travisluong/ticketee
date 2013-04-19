031afbcfc597a14047b56984ae927bfddcbb4d10
Travis Luong <travis@travisluong.com>
2013-04-19 16:44:52 -0700

    implement edit action for the tickets controller

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :name
-  has_many :tickets
+  has_many :tickets, :dependent => :delete_all
 
   validates :name, :presence => true
 end
